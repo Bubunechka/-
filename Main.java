a7c1c1a19dcf4713446581f664c67da5561aef92
Bubunechka <110813295+Bubunechka@users.noreply.github.com>
2024-04-11 17:43:04 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
 
   public static void main (String[] args){
 
-    //Game game = new Game();
+    Game game = new Game();
     
     System.out.printin("Начать новую игру?");
     
