aaeb5930f96168c04c64407a2350d8902cec41a2
Bubunechka <110813295+Bubunechka@users.noreply.github.com>
2024-04-11 14:54:16 +0300

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game{
+
+  String newGame;
+  String out;
+
+
+  // 1. Делаем список слов
+  
+  List<String> listWords = new ArrayList<>();
+        words.add("человек");
+        words.add("день");
+        words.add("место");
+        // Добавьте остальные слова в список
+ //List<String> list = List.of("элемент1", "элемент2", "элемент3", "элемент4", "элемент5");
+
+// 2. Делаем рандомную выборку
+
+Random random = new Random();
+Strig randomWord = listWords.get(random.nextInt(list.size()));
+
+// 3. "сохраняем" выбранное слово, которое будем разгадывать 
+
+public void changeVord(words){
+  ???
+}
+
+
+// ИНФО:
+// Сделать рандомную выборку слова при "новой игре"
+
+        // // Примеры операций со списком
+        // System.out.println(words.get(0)); // Выводит первое слово в списке
+        // System.out.println(words.size()); // Выводит размер списка (количество слов)
+
+        // Другие операции, такие как удаление элементов, обновление и т.д., также доступны для списка.
+
+        // // Вывод всех слов в списке
+        // for (String word : words) {
+        //     System.out.println(word);
+        // }
+  
+  
+}
